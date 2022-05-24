9d83f526a9486d848db8043262c8319fbd4c83f6
Sayo Law <shayolaw@gmail.com>
2022-05-24 18:55:33 -0400

    Displays User Location in text field At the bottom of the screen

--- A1_A2_iOS_ Olaoluwa_C0847663/ViewController.swift
+++ A1_A2_iOS_ Olaoluwa_C0847663/ViewController.swift
@@ -6,13 +6,86 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate  {
+    var locationManager = CLLocationManager()
+    var lat = CLLocationDegrees()
+    var lng = CLLocationDegrees()
+    @IBOutlet weak var displayArea: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        mapView.isZoomEnabled = false
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        lat = location?.coordinate.latitude ?? 0.0
+        lng = location?.coordinate.longitude ?? 0.0
+//        displayLocation(latitude:lat, longitude: lng, title: "User", subtitile: "My location")
+        if let location = location {
+            CLGeocoder().reverseGeocodeLocation(location){ placemarks, error in
+                if error != nil{
+                    print(error!)
+                }
+                else{
+                    DispatchQueue.main.async {
+                        if let placemark = placemarks?[0]{
+                            var address = ""
+                            if placemark.subThoroughfare != nil {
+                                address += placemark.subThoroughfare! + " "
+                            }
+                            if placemark.thoroughfare != nil {
+                                address += placemark.thoroughfare! + "\n "
+                            }
+                            if placemark.subLocality != nil {
+                                address += placemark.subLocality! + "\n"
+                            }
+                            if placemark.administrativeArea != nil {
+                                address += placemark.administrativeArea! + ","
+                            }
+                            if placemark.postalCode != nil {
+                                address += placemark.postalCode! + "\n"
+                                print(placemark.postalCode)
+                            }
+                            if placemark.country != nil {
+                                address += placemark.country! + " "
+                                print(address)
+                            }
+                            self.displayArea.numberOfLines = 0
+                          self.displayArea.text = address
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
+
+    }
+    func displayLocation(
+        latitude:CLLocationDegrees,
+        longitude:CLLocationDegrees,
+        title: String,
+        subtitile: String){
+            print(latitude)
+            let latDelta: CLLocationDegrees = 0.05
+            let lngDelat:CLLocationDegrees = 0.05
+            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelat)
+            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//            let region = MKCoordinateRegion(center: location, span: span)
+//            mapView.setRegion(region, animated: true)
+    
+    }
+
 
 
 }
