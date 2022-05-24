589f737cb10310b52e0173ecd1f8479e8e2767df
Sayo Law <shayolaw@gmail.com>
2022-05-24 17:55:31 -0400

    Initial Commit

---
+++ A1_A2_iOS_ Olaoluwa_C0847663/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_ Olaoluwa_C0847663
+//
+//  Created by Sayo Lawal on 2022-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
